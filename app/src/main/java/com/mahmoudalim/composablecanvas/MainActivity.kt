1892f38e9b90c2d42180e5a9a11addd5fc604353
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2021-10-02 19:51:48 +0200

    add 2 new shapes with canvas using drawArc -> progressbar , packman

--- app/src/main/java/com/mahmoudalim/composablecanvas/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/composablecanvas/MainActivity.kt
@@ -11,8 +11,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import com.mahmoudalim.composablecanvas.ui.theme.ComposableCanvasTheme
 
@@ -24,32 +28,61 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     FirstCanvas()
+                    DrawCanvas(Shape.PackMan)
+                    DrawCanvas(Shape.ProgressBar)
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun FirstCanvas() {
-    Canvas(
-        modifier = Modifier
-            .size(300.dp)
-            .padding(20.dp)
-    ) {
-        drawRect(
-            color = Color.Blue,
-            size = size
-        )
-        drawCircle(
-            brush = Brush.radialGradient(
-                colors = listOf(Color.Red, Color.Yellow),
-                center = center,
-                radius = 50.dp.toPx()
-            ),
-            radius = 50.dp.toPx(),
-            center = center
-        )
+    enum class Shape {
+        PackMan,
+        ProgressBar;
+    }
 
+    @Composable
+    private fun DrawCanvas(shape: Shape) {
+        Canvas(
+            modifier = Modifier
+                .size(60.dp)
+                .offset(if (shape == Shape.ProgressBar) 50.dp else 0.dp)
+        ) {
+            drawArc(
+                color = Color.Green,
+                startAngle = 0f,
+                sweepAngle = 275f,
+                useCenter = shape == Shape.PackMan,
+                topLeft = Offset(40f, 40f),
+                style = if (shape == Shape.ProgressBar) Stroke(width = 3.dp.toPx()) else Fill
+            )
+
+
+        }
+    }
+
+
+    @Composable
+    fun FirstCanvas() {
+        Canvas(
+            modifier = Modifier
+                .size(300.dp)
+                .padding(40.dp)
+        ) {
+            drawRect(
+                color = Color.Blue,
+                size = size
+            )
+            drawCircle(
+                brush = Brush.radialGradient(
+                    colors = listOf(Color.Red, Color.Yellow),
+                    center = center,
+                    radius = 50.dp.toPx()
+                ),
+                radius = 50.dp.toPx(),
+                center = center
+            )
+
+        }
     }
 }
